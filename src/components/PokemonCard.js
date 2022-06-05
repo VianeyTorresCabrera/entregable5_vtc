2386d18704f50bcbe24a27d9be8b5bd211e510e9
VianeyTorresCabrera <102828830+VianeyTorresCabrera@users.noreply.github.com>
2022-06-05 02:39:32 -0500

    mas commit

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ValidateColor from "./Color_card";
-import Pokedex from "./Pokedex";
+
 
 const PokemonCard = ({characterUrl}) => {
 
